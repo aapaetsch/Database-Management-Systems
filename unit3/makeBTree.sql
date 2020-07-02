67df899ead332d1eea9b95b89e611c7072a5c724
Aidan Paetsch <aapaetsch@gmail.com>
2020-07-02 17:56:23 -0600

    all project files

--- unit3/makeBTree.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX btree_areaMBR_minX ON areaMBR(minX);
-CREATE INDEX btree_areaMBR_minY ON areaMBR(minY);
-CREATE INDEX btree_areaMBR_maxX ON areaMBR(maxX);
-CREATE INDEX btree_areaMBR_maxY ON areaMBR(maxY);
--- unit3/makeRTree.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIRTUAL TABLE rtree_areaMBR USING rtree(
-	id, 
-	minX, maxX,
-	minY, maxY
-);
-INSERT INTO rtree_areaMBR 
-SELECT id, minX, maxX, minY, maxY
-FROM areaMBR;
- 
